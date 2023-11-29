ce8ba54e6426e68e3bb65de2442debf5349715ec
Jan Philipp Vogtherr <jan-philipp.vogtherr@byteways.de>
2023-11-30 00:31:49 +0100

    ofl-ide-plugin

---
+++ ofl-ide-plugin/src/main/kotlin/ofl/ide/plugin/HelloWorldAction.kt
@@ -0,0 +1,10 @@
+package ofl.ide.plugin
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class HelloWorldAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
